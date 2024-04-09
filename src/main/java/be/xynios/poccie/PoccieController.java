3d8c2e5615d4d3b4d31446a16cd5e2ce54425ad1
saeysdi <dimitri.saeys@vrt.be>
2024-04-09 20:08:18 +0200

    env var

--- src/main/java/be/xynios/poccie/PoccieController.java
+++ src/main/java/be/xynios/poccie/PoccieController.java
@@ -3,13 +3,21 @@ package be.xynios.poccie;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 public class PoccieController {
 
     @GetMapping("/greeting")
+
     public Map<String, String> hello(){
-        return Map.of("Hello", "boys");
+        Map<String, String> resp = new HashMap<>();
+        resp.put("Hello", "Boys");
+        String who = System.getenv("who");
+        if (who != null) {
+            resp.put("and", who);
+        }
+        return resp;
     }
 }
